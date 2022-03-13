533178d83e9b1481dd345c5008e4dc2f47fb0ac6
DeveloperRooney <han_0427@naver.com>
2022-03-13 22:33:45 +0900

    회원 업데이트 API 생성

--- src/main/java/com/jpabook/jpashop/api/MemberApiController.java
+++ src/main/java/com/jpabook/jpashop/api/MemberApiController.java
@@ -2,11 +2,10 @@ package com.jpabook.jpashop.api;
 
 import com.jpabook.jpashop.domain.Member;
 import com.jpabook.jpashop.service.MemberService;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
@@ -24,7 +23,7 @@ public class MemberApiController {
         return new CreateMemberResponse(id);
     }
 
-    @PostMapping("/api/v2/membser")
+    @PostMapping("/api/v2/members")
     // 엔티티를 사용하지 않고 API에서 사용할 Dto 역할 클래스를 사용함으로써 엔티티 변경에도 API 스펙에는 변화가 없도록 함
     public CreateMemberResponse saveMemberV2(@RequestBody @Valid CreateMemberRequest request) {
         Member member = new Member();
@@ -32,6 +31,28 @@ public class MemberApiController {
 
         Long id = memberService.join(member);
         return new CreateMemberResponse(id);
+    }
+
+    @PutMapping("/api/v2/members/{id}")
+    public UpdateMemberResponse updateMemberV2(
+            @PathVariable("id") Long id,
+            @RequestBody @Valid UpdateMemberRequest request) {
+
+        memberService.update(id, request.getName());
+        Member findMember = memberService.findOne(id);
+        return new UpdateMemberResponse(findMember.getId(), findMember.getName());
+    }
+
+    @Data
+    static class UpdateMemberRequest {
+        private String name;
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class UpdateMemberResponse {
+        private Long id;
+        private String name;
     }
 
     @Data
@@ -48,4 +69,6 @@ public class MemberApiController {
             this.id = id;
         }
     }
+
+
 }
