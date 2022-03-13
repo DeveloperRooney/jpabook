6a609392dd03b7d04cc30626b6c9e28234da80d6
DeveloperRooney <han_0427@naver.com>
2022-03-13 17:06:31 +0900

    주문 목록, 주문 취소 기능 구현

--- src/main/java/com/jpabook/jpashop/controller/OrderController.java
+++ src/main/java/com/jpabook/jpashop/controller/OrderController.java
@@ -2,15 +2,14 @@ package com.jpabook.jpashop.controller;
 
 import com.jpabook.jpashop.domain.Member;
 import com.jpabook.jpashop.domain.item.Item;
+import com.jpabook.jpashop.repository.OrderSearch;
 import com.jpabook.jpashop.service.ItemService;
 import com.jpabook.jpashop.service.MemberService;
 import com.jpabook.jpashop.service.OrderService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -39,6 +38,21 @@ public class OrderController {
                         @RequestParam("count") int count) {
 
         orderService.order(memberId, itemId, count);
+        return "redirect:/orders";
+    }
+
+    @GetMapping("/orders")
+    public String orderList(@ModelAttribute("orderSearch") OrderSearch orderSearch,
+                            Model model) {
+
+        model.addAttribute("orders", orderService);
+
+        return "order/orderList";
+    }
+
+    @PostMapping("/orders/{orderId}/cancel")
+    public String cancelOrder(@PathVariable("orderId") Long orderId) {
+        orderService.cancelOrder(orderId);
         return "redirect:/orders";
     }
 }
