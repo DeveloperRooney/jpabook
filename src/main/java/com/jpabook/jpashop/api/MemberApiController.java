7c75734ed717bf7a72582e7706a3e4f8714633e9
DeveloperRooney <han_0427@naver.com>
2022-03-13 17:47:27 +0900

    회원 가입을 위한 API를 엔티티를 직접 사용하는 V1과 DTO를 사용하는 V2로 나누어서 작성

--- src/main/java/com/jpabook/jpashop/api/MemberApiController.java
+++ src/main/java/com/jpabook/jpashop/api/MemberApiController.java
@@ -17,9 +17,27 @@ public class MemberApiController {
     private final MemberService memberService;
 
     @PostMapping("/api/v1/members")
+    // 엔티티로 API를 호출하면 엔티티 변경 시 API 스펙에 문제가 발생할 수 있다
+    // API를 만들 떄 엔티티 사용하지 말 것
     public CreateMemberResponse saveMemberV1(@RequestBody @Valid Member member) {
         Long id = memberService.join(member);
         return new CreateMemberResponse(id);
+    }
+
+    @PostMapping("/api/v2/membser")
+    // 엔티티를 사용하지 않고 API에서 사용할 Dto 역할 클래스를 사용함으로써 엔티티 변경에도 API 스펙에는 변화가 없도록 함
+    public CreateMemberResponse saveMemberV2(@RequestBody @Valid CreateMemberRequest request) {
+        Member member = new Member();
+        member.setName(request.getName());
+
+        Long id = memberService.join(member);
+        return new CreateMemberResponse(id);
+    }
+
+    @Data
+    // API 스펙에 맞게 작성하면 엔티티를 변경하더라도 문제가 발생하지 않음
+    static class CreateMemberRequest {
+        private String name;
     }
 
     @Data
